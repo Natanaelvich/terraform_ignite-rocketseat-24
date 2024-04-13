b9d2cddc852e14eeefe4e666ceeeb82f01ff6deb
Natanael <taelima1997@gmail.com>
2024-04-13 20:15:33 -0300

    Update AWS S3 bucket resource configuration in main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "s3_bucket" {
-  bucket = "rocketseat-bucket-iac-${terraform.workspace}"
+  bucket = "natanael-bucket-iac-${terraform.workspace}"
 
   tags = {
     Name    = "Primeiro bucket"
