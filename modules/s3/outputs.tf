7e4d1c010e7a393890af0f5761c8334e2f1c83dd
Natanael <taelima1997@gmail.com>
2024-04-14 07:23:06 -0300

    add module s3

---
+++ modules/s3/outputs.tf
@@ -0,0 +1,10 @@
+output "bucket_domain_name" {
+  value       = data.aws_s3_bucket.bucket.bucket_domain_name
+  sensitive   = false
+  description = "Nome de domínio do bucket S3"
+}
+output "bucket_id" {
+  value       = data.aws_s3_bucket.bucket.id
+  sensitive   = false
+  description = "ID de domínio do bucket S3"
+}
