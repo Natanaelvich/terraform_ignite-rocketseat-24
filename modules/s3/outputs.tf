a2cc611bfae2be90940c6182c9b0da931b75c2e3
Natanael <taelima1997@gmail.com>
2024-04-14 10:09:23 -0300

    Update AWS S3 bucket resource configuration in outputs.tf

--- modules/s3/outputs.tf
+++ modules/s3/outputs.tf
@@ -1,11 +1,11 @@
-# output "bucket_domain_name" {
-#   value       = aws_s3_bucket.s3_bucket.bucket_domain_name
-#   sensitive   = false
-#   description = "Nome de domínio do bucket S3"
-# }
+output "bucket_domain_name" {
+  value       = aws_s3_bucket.bucket.bucket_domain_name
+  sensitive   = false
+  description = "Nome de domínio do bucket S3"
+}
 
-# output "bucket_region" {
-#   value       = aws_s3_bucket.s3_bucket.region
-#   sensitive   = false
-#   description = "Região do bucket"
-# }
+output "bucket_id" {
+  value       = aws_s3_bucket.bucket.id
+  sensitive   = false
+  description = "Id do bucket"
+}
