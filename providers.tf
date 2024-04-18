c1c0b29c076d39b512e5875e3237420ea4984af1
Natanael <taelima1997@gmail.com>
2024-04-18 20:09:36 -0300

    Add backend configuration for Terraform state in providers.tf

--- providers.tf
+++ providers.tf
@@ -5,9 +5,25 @@ terraform {
       version = "5.36.0"
     }
   }
+
+   backend "s3" {
+    bucket  = "natanael-state-bucket-tf"
+    profile = "natanael-profile"
+    region  = "us-west-1"
+    key     = "terraform.tfstate"
+    encrypt = true
+  }
 }
 
 provider "aws" {
   region  = "us-west-1"
   profile = "natanael-profile"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = var.state_bucket
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
